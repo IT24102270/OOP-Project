b126226fe23c16462cf1dcd6703cfa05257c1642
IT24102270 <it24102270@my.sliit.lk>
2025-03-24 06:12:26 +0530

    "Added a hashing method, changed methods and variables to camelCase"

--- src/main/java/com/drivingschool/model/User.java
+++ src/main/java/com/drivingschool/model/User.java
@@ -1,96 +1,121 @@
 package main.java.com.drivingschool.model;
+import java.security.NoSuchAlgorithmException;
+import java.security.MessageDigest;
 
 public class User {
-    private String username;
-    private String userid;
-    private String useraddress;
-    private String phonenumber;
-    private String useremail;
+    private String userName;
+    private String userId;
+    private String userAddress;
+    private String phoneNumber;
+    private String userEmail;
     private String password;
     private String role;
 
-    public User(String username, String userid, String useraddress, String phonenumber, String useremail, String password, String role) {
-        this.username = username;
-        this.userid = userid;
-        this.useraddress = useraddress;
-        this.phonenumber = phonenumber;
-        this.useremail = useremail;
-        this.password = password;
+    public User(String userName, String userId, String userAddress, String phoneNumber, String userEmail, String password, String role) {
+        this.userName = userName;
+        this.userId = userId;
+        this.userAddress = userAddress;
+        this.phoneNumber = phoneNumber;
+        this.userEmail = userEmail;
+        this.password = hashPassword(password);
         this.role = role;
     }
 
     // getters
     public String getUsername() {
-        return username;
+        return userName;
     }
     public String getUserid() {
-        return userid;
+        return userId;
     }
     public String getUseremail() {
-        return useremail;
+        return userEmail;
     }
     public String getPhonenumber() {
-        return phonenumber;
+        return phoneNumber;
     }
     public String getRole() {
         return role;
     }
 
     //setters
-    public void setUsername(String username) {
-        this.username = username; // username allocation to be added , check with existing usernames
+    public void setUsername(String userName) {
+        this.userName = userName; // username allocation to be added , check with existing usernames
     }
-    public void setUserid(String userid) {
-        this.userid = userid;    // user id should be automatically generated
+    public void setUserid(String userId) {
+        this.userId = userId;    // user id should be automatically generated
     }
-    public void setPhonenumber(String phonenumber) {
-        this.phonenumber = phonenumber;
+    public void setPhonenumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
-    public void setUseremail(String useremail) {
-        this.useremail = useremail;
+    public void setUseremail(String userEmail) {
+        this.userEmail = userEmail;
     }
     public void setPassword(String password) {
-        this.password = password; // password should be hashed and saved
+        this.password = hashPassword(password); // password should be hashed and saved
     }
     public void setRole(String role) {
         this.role = role;
     }
 
+    //Hashing method
+    private String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            byte[] hashedBytes = md.digest(password.getBytes());
+
+            //bytes >>> hexadecimal String
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hashedBytes) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+        }
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("HAshing algorithm not found",e);
+        }
+    }
+
     // login logout
-    public boolean login(String username, String password) {
-        return this.username.equals(username) && this.password.equals(password);
+    public boolean login(String userName, String password) {
+        return this.userName.equals(userName) && this.password.equals((hashPassword(password)));
     }
     public void logout() {
         System.out.println("User Logged out");
     }
 
     // other operations
-    public void createuser() {
-        System.out.println("Hi "+username);  // welcome msg should be added
+    public void createUser() {
+        System.out.println("Hi "+userName);  // welcome msg should be added
     }
-    public void displayuser() {
-        
+    public void displayUser() {
+        System.out.println("Username "+userName);
+        System.out.println("User id "+userId);
+        System.out.println("User address "+userAddress);
+        System.out.println("Phone number "+phoneNumber);
+        System.out.println("User email "+userEmail);
+
     }
-    public void changePassword(String currentpassword, String newpassword) {
-        if (password.equals(currentpassword)) {   // password hashes to be used
-            this.password = newpassword;          // password hashes to be used
+    public void changePassword(String currentPassword, String newPassword) {
+        if (password.equals(hashPassword(currentPassword))) {   // password hashes to be used
+            this.password = hashPassword(newPassword);          // password hashes to be used
             System.out.println("Password changed successfully!");
         }
         else {
             System.out.println("Passwords do not match");
         }
     }
-    public void changeUsername(String username) {
-        this.username = username;
+    public void changeUsername(String userName) {
+        this.userName = userName;
     }
-    public void changephonenumber(String phonenumber) {
-        this.phonenumber = phonenumber;
+    public void changePhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
-    public void changeaddress(String address) {
-        this.useraddress = address;
+    public void changeAddress(String address) {
+        this.userAddress = address;
     }
-    public void changeemail(String email) {
-        this.useremail = email;
+    public void changeEmail(String email) {
+        this.userEmail = email;
     }
     public void deleteUser() {
         System.out.println("User has been removed!");
