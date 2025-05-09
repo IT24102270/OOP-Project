0fce8ccd407cb53002c65bc7e9556d8e54b4a75e
IT24102320 <it24102320@my.sliit.lk>
2025-05-09 19:56:10 +0530

    Update ReviewServlet.java

--- src/main/java/com/drivingschool/servlets/ReviewServlet.java
+++ src/main/java/com/drivingschool/servlets/ReviewServlet.java
@@ -1,4 +1,43 @@
 package com.drivingschool.servlets;
 
-public class ReviewServlet {
+import com.drivingschool.model.Review;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/ReviewServlet")
+public class ReviewServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String action = req.getParameter("action");
+
+        try {
+            switch (action) {
+                case "addReview":
+                case "updateReview":
+                    Review review = new Review(
+                            req.getParameter("reviewID"),
+                            req.getParameter("userID"),
+                            req.getParameter("comment"),
+                            Integer.parseInt(req.getParameter("rating"))
+                    );
+                    if (action.equals("addReview")) {
+                        Review.addReview(review);
+                    } else {
+                        Review.updateReview(review);
+                    }
+                    break;
+
+                case "deleteReview":
+                    Review.deleteReview(req.getParameter("reviewID"));
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        res.sendRedirect("reviewDashboard.jsp");
+    }
 }
