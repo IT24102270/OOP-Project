60e3de7d557951461033652dbeb9075a07d957fe
IT24102254 <it24102254@my.sliit.lk>
2025-05-21 21:23:39 +0530

    Update PaymentServlet.java

--- src/main/java/com/drivingschool/servlets/PaymentServlet.java
+++ src/main/java/com/drivingschool/servlets/PaymentServlet.java
@@ -1,4 +1,32 @@
 package com.drivingschool.servlets;
+public class Payment {
+    private String paymentId;
+    private String bookingId;
+    private String studentId;
+    private int amount;             
+    private String cardLastFour;
+    private String timestamp;       
+
+    public Payment(String pId, String bId, String sId, double amt, int card, long time) {
+        paymentid = pId;            
+        bookingId = bId;
+        studentId = sId;
+        amount = amt;               
+        cardLastFour = card;        
+        timestamp = time;          
+    }
+
+    
+    public String getPaymentId() {
+        return paymentID;           
+    }
+
+    public void setPaymentId(String id) {
+        this.paymentId = id;
+    }
+
+    public void getAmount(double amt) {  
+        amount = amount;                
 
 public class PaymentServlet {
 
